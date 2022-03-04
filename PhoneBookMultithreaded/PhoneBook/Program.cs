10cafa881c715c713b44cc9398d59908781452bc
Jacob Timms <jacob_tims@live.com>
2022-03-04 23:20:07 +0000

    revised exception Handling

--- PhoneBookMultithreaded/PhoneBook/Program.cs
+++ PhoneBookMultithreaded/PhoneBook/Program.cs
@@ -21,147 +21,135 @@ namespace PhoneBook
 
             while (true)
             {
-                Console.WriteLine("\nEnter command:");
-                var input = Console.ReadLine();
-
-                //EXIT COMMAND
-                if (input.Equals("EXIT"))
+                try
                 {
-                    return;
-                }
+                    Console.WriteLine("\nEnter command:");
+                    var input = Console.ReadLine();
+                    string[] userInputs = input.Split(' ');
 
-                //SPLIT USER INPUT STRING
-                string[] userInputs = input.Split(' ');
+                    switch (userInputs[0])
+                    {
+                        case "EXIT":
+                            return;
 
-                //CALL STORE COMMAND
-                if (userInputs[0] == "STORE")
-                {
-                    if (userInputs.Length < 3)
-                    {
-                        Console.WriteLine("Name / Phone Number is empty");
-                    }
-                    else
-                    {
-                        try
-                        {
+                        case "STORE":
+                            if (userInputs.Length < 3)
+                            {
+                                throw new EmptyFieldException();
+                            }
+
                             Phonebook.Store(userInputs[1], userInputs[2]);
                             WriteToFile.writeTextFileToDictionary(Phonebook.Contacts);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
-                    }
-                }
+                            break;
 
-                //CALL GET COMMAND
-                else if (userInputs[0] == "GET")
-                {
-                    if(userInputs.Length < 2)
-                    {
-                        Console.WriteLine("Name is empty");
-                    }
-                    else if (userInputs.Length > 2)
-                    {
-                        Console.WriteLine("Name cannot include spaces");
-                    }
-                    else
-                    {
-                        try
-                        {
+                        case "GET":
+                            if (userInputs.Length < 2)
+                            {
+                                throw new EmptyFieldException("Name is empty");
+                            }
+                            if (userInputs.Length > 2)
+                            {
+                                throw new InvalidInputException("Name cannot include spaces");
+                            }
+
                             Phonebook.Get(userInputs[1]);
                             WriteToFile.writeTextFileToDictionary(Phonebook.Contacts);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
-                    }
-                }
+                            break;
 
-                //CALL DEL COMMAND
-                else if (userInputs[0] == "DEL")
-                {
-                    long phoneNum;
-
-                    if (userInputs.Length < 2)
-                    {
-                        Console.WriteLine("Name / Phone Number is empty");
-                    }
-                    else if (userInputs.Length > 2)
-                    {
-                        Console.WriteLine("Name / Phone Number cannot include spaces");
-                    }
-                    else if (Int64.TryParse(userInputs[1], out phoneNum))
-                    {
-                        if(userInputs[1].Length != 11)
-                        {
-                            Console.WriteLine("Phone Number must be 11 digits");
-                        }
-                        else
-                        {
-                            try
+                        case "DEL":
+                            long phoneNum;
+                            if (Int64.TryParse(userInputs[1], out phoneNum))
                             {
+                                if (userInputs.Length < 2)
+                                {
+                                    throw new EmptyFieldException();
+                                }
+                                if (userInputs.Length > 2)
+                                {
+                                    throw new InvalidInputException();
+                                }
+                                if (userInputs[1].Length != 11)
+                                {
+                                    throw new ArgumentOutOfRangeException("Phone Number must be 11 digits");
+                                }
+
                                 Phonebook.Del(phoneNum);
                                 WriteToFile.writeTextFileToDictionary(Phonebook.Contacts);
                             }
-                            catch (Exception e)
+                            else
                             {
-                                Console.WriteLine(e.Message);
+                                Phonebook.Del(userInputs[1]);
+                                WriteToFile.writeTextFileToDictionary(Phonebook.Contacts);
                             }
-                        }
-                    }
-                    else
-                    {
-                        try
-                        {
-                            Phonebook.Del(userInputs[1]);
-                            WriteToFile.writeTextFileToDictionary(Phonebook.Contacts);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
-                    }
-                }
+                            break;
 
-                //CALL UPDATE COMMAND
-                else if (userInputs[0] == "UPDATE")
-                {
-                    if (userInputs.Length < 3)
-                    {
-                        Console.WriteLine("Name / Phone Number is empty");
-                    }
-                    else if(userInputs.Length > 3)
-                    {
-                        Console.WriteLine("Name / Phone Number cannot include spaces");
-                    }
-                    else
-                    {
-                        try
-                        {
+                        case "UPDATE":
+                            if (userInputs.Length < 3)
+                            {
+                                throw new EmptyFieldException();
+                            }
+                            if (userInputs.Length > 3)
+                            {
+                                throw new InvalidInputException();
+                            }
+
                             Phonebook.Update(userInputs[1], userInputs[2]);
                             WriteToFile.writeTextFileToDictionary(Phonebook.Contacts);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
+                            break;
+
+                        case "HELP":
+                            Console.WriteLine("\nSTORE: \nTo store a phone number, type 'STORE' followed by a single name (do not include any spaces) & phone number: 'STORE Name 12345678987'");
+                            Console.WriteLine("GET: \nTo get an existing phone number, type 'GET' followed by the contact's name: 'GET Name'");
+                            Console.WriteLine("DEL: \nTo delete an existing contact, type 'DEL' followed by the contact's name or phone number: 'DEL Name' or 'DEL 12345678987'");
+                            Console.WriteLine("UPDATE: \nTo update an existing contact's phone number, type 'UPDATE' followed by the contact's name and the new phone nmuber: 'UPDATE Name 12345678987'");
+                            Console.WriteLine("EXIT: \nTo exit the PhoneBook application, type 'EXIT");
+                            break;
+
+                        default:
+                            throw new ArgumentOutOfRangeException("Please input a valid command, type 'HELP' to show examples");
                     }
                 }
-                else if (userInputs[0] == "HELP" && userInputs.Length == 1)
+                catch (Exception e)
                 {
-                    Console.WriteLine("\nSTORE: \nTo store a phone number, type 'STORE' followed by a single name (do not include any spaces) & phone number: 'STORE Name 12345678987'");
-                    Console.WriteLine("GET: \nTo get an existing phone number, type 'GET' followed by the contact's name: 'GET Name'");
-                    Console.WriteLine("DEL: \nTo delete an existing contact, type 'DEL' followed by the contact's name or phone number: 'DEL Name' or 'DEL 12345678987'");
-                    Console.WriteLine("UPDATE: \nTo update an existing contact's phone number, type 'UPDATE' followed by the contact's name and the new phone nmuber: 'UPDATE Name 12345678987'");
-                    Console.WriteLine("EXIT: \nTo exit the PhoneBook application, type 'EXIT");
-                }
-                else
-                {
-                    Console.WriteLine("Please input a valid command, type 'HELP' to show examples");
+                    Console.WriteLine(e.Message);
                 }
             }
+        }
+    }
+
+    public class EmptyFieldException : Exception
+    {
+        public EmptyFieldException()
+            : base("Name / Phone Number is empty")
+        {
+        }
+
+        public EmptyFieldException(string message)
+            : base(message)
+        {
+        }
+
+        public EmptyFieldException(string message, Exception inner)
+    : base(message, inner)
+        {
+        }
+    }
+
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException()
+            : base("Name / Phone Number cannot include spaces")
+        {
+        }
+
+        public InvalidInputException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidInputException(string message, Exception inner)
+    : base(message, inner)
+        {
         }
     }
 }
